47b0c950d27737bcf54805351cdb365f15c74aac
Cody Karjadi <ckarjadi@bu.edu>
2020-04-23 12:43:34 -0400

    added functionality for opening of WAV and WMA files. when a single file is selected, its path is simply printed

--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -1,4 +1,37 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+
+import java.io.File;
+
 public class MenuController {
+    @FXML
+    private AnchorPane anchor;
+    @FXML
+    private MenuItem openMI;
+    private File workingDir = null;
+
+    public void openAction(ActionEvent actionEvent) {
+        Window stage = anchor.getScene().getWindow();
+        if (workingDir == null) {
+            workingDir = new File("." + File.separator + "file");
+        } else {
+            while (!workingDir.exists()) {
+                workingDir = new File(workingDir.getParent());
+            }
+        }
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("audio files (wav, wma)",
+                        "*.wav", "*.wma"));
+        fileChooser.setInitialDirectory(workingDir);
+        fileChooser.setTitle("Open Audio File");
+        File selectedFile = fileChooser.showOpenDialog(stage);
+        System.out.println(selectedFile);
+    }
 }
